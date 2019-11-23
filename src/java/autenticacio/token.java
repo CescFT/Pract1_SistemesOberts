06ceaa2883596e80f63115271491f6d8c816d51d
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-11-23 19:19:34 +0100

    master: added token class.

---
+++ src/java/autenticacio/token.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autenticacio;
+import javax.persistence.*;
+import java.io.Serializable;
+import javax.validation.constraints.Size;
+/**
+ *
+ * @author Cesc
+ */
+@Embeddable
+public class token implements Serializable{
+    @Column(name="TOKEN") @Size(max=500)
+    private String tokenAutoritzacio;
+
+    public token(){
+        
+    }
+    public token(String token) {
+        this.tokenAutoritzacio = token;
+    }
+
+    public String getTokenAutoritzacio() {
+        return tokenAutoritzacio;
+    }
+
+    public void setTokenAutoritzacio(String tokenAutoritzacio) {
+        this.tokenAutoritzacio = tokenAutoritzacio;
+    }
+
+    @Override
+    public String toString() {
+        return "token{" + "tokenAutoritzacio=" + tokenAutoritzacio + '}';
+    }
+    
+    
+}
